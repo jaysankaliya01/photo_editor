68f69b56288d701527882f6de5621d45b0b87c82
Raj-Pandavadara <158834378+Raj-Pandavadara@users.noreply.github.com>
2024-11-06 15:24:49 +0530

    Update enhance_download.dart

--- lib/enhance_download.dart
+++ lib/enhance_download.dart
@@ -11,13 +11,7 @@ class EnhancePhotoDownloadPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context),
         ),
-        title: const Text("AI Photo Enhance"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
+   
               padding: const EdgeInsets.all(16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
